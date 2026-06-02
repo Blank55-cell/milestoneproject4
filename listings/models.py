9cf23ea5d4ed2d4ed26ed9f746b5b49da294b0b6
Carlos <carlosalba101@proton.me>
2026-06-02 04:03:36 +0100

    added in the first table formy backend db

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    price = models.IntegerField()  # Stored as a whole number (e.g., 450000)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    is_featured = models.BooleanField(default=False) # Useful for filtering home page items later
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Properties"
