be6f5309c2bab010f88227471e6ce8678e15dc17
Carlos <carlosalba101@proton.me>
2026-06-02 04:27:52 +0100

    added in admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Property
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'location', 'is_featured')
+    list_filter = ('is_featured', 'bedrooms', 'bathrooms')
+    search_fields = ('title', 'location')
