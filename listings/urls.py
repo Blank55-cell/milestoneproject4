acecb952cd87278ad880cfe123d417e16d893edc
Carlos <carlosalba101@proton.me>
2026-06-08 23:02:31 +0100

    updated the  listings urls

--- listings/urls.py
+++ listings/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),                      # Homepage
     path('listings/', views.all_listings, name='listings'),      # Show all listings page
+    path('listings/<int:listing_id>/', views.listing_detail, name='listing_detail'), # Individual property details
+    path('account/', views.account_dashboard, name='account_dashboard'), # Logged in user dashboard
 ]
