63c307cd865fe1ee58920c14b3fdc54337ffaa6a
Carlos <carlosalba101@proton.me>
2026-06-08 19:37:48 +0100

    Updated the models.py

--- listings/models.py
+++ listings/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
 
 class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     location = models.CharField(max_length=200)
-    price = models.IntegerField()  # Stored as a whole number (e.g., 450000)
+    price = models.IntegerField()
     bedrooms = models.IntegerField()
     bathrooms = models.IntegerField()
-    is_featured = models.BooleanField(default=False) # Useful for filtering home page items later
+    is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -18,3 +16,13 @@ class Property(models.Model):
 
     class Meta:
         verbose_name_plural = "Properties"
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='bookings')
+    booking_date = models.DateTimeField(auto_now_add=True)
+    stripe_payment_id = models.CharField(max_length=250, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.property.title}"
