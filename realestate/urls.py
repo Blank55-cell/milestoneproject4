ab683cf0f328ae30cc787c87c95bce3a546854fc
Carlos <carlosalba101@proton.me>
2026-06-11 02:44:42 +0100

    add static url routing fallback for production

--- realestate/urls.py
+++ realestate/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('listings.urls')),            
     path('accounts/', include('allauth.urls')),   
 ]
+
+# Ensures Django hands off static file requests to WhiteNoise when DEBUG is False
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
