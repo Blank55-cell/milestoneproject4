5985c0024dd7fa6dcac1d9eb17be7c15fdc8e439
Carlos <carlosalba101@proton.me>
2026-06-15 20:22:45 +0100

    Updated urls for my accounts section

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # My main profile dashboard view when I'm logged in
     path('', views.account_dashboard, name='account_dashboard'),
+    
+    # URL for toggling a property to my wishlist
+    path('toggle-save/<int:listing_id>/', views.toggle_save_property, name='toggle_save'),
 ]
