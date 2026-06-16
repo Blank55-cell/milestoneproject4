9ff85640f44a631206e6e75475127050b930fec5
Carlos <carlosalba101@proton.me>
2026-06-16 02:47:02 +0100

    Added in command to automatically verify userss

---
+++ listings/management/commands/verify_emails.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from allauth.account.models import EmailAddress
+
+
+class Command(BaseCommand):
+    help = "Mark all existing email addresses as verified"
+
+    def handle(self, *args, **kwargs):
+        updated = 0
+        for email in EmailAddress.objects.filter(verified=False):
+            email.verified = True
+            email.primary = True
+            email.save()
+            updated += 1
+            self.stdout.write(self.style.SUCCESS(f"Verified: {email.email}"))
+
+        self.stdout.write(self.style.SUCCESS(f"Done. {updated} accounts verified."))
